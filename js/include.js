95e8d1d3b3e1dff745d66297d539a04d9e74f5ad
LiYongLi <981658468@qq.com>
2018-09-30 10:26:05 +0800

    no message

--- js/include.js
+++ js/include.js
@@ -1,5 +1,5 @@
 //$("head").load('common/head.html')
 //$("body").load('common/menus.html')
 var html=document.documentElement;
-var width=html.getBoundingClientRect().width; //获取屏幕宽度的另一个方法
+var width=html.getBoundingClientRect().width; 
 html.style.fontSize=width/15+"px";
